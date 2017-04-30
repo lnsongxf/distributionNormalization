97128c60d82469de302cd2679b0a773c936e269b
tzuohann <tzuohann@gmail.com>
2017-04-30 18:52:17 -0400

    wokring on a mac now. can be used to compute results for two production functions after the setup

--- Source/getxyWageCount.m
+++ Source/getxyWageCount.m
@@ -11,14 +11,30 @@ function [xyWageCount,SigmaNoise_Hat] = getxyWageCount(SimWage,yBy,HiredWorkerNa
     WW                        = HiredWorkerWage(Temp,:);
     %WageCount                is [AverageWage,Counts]
     %VecTemp                  is [Wage - AverageWage]
-    [WageCount,VecTemp,NumM]  = wageStats(IW,WW,int32(C.NumAgentsSim));
+    %Wagecount indexed by worker name
+    
+    %[WageCount,VecTemp,NumM]  = wageStats(IW,WW,int32(C.NumAgentsSim));
+    
+    idx                       = IW > 0;
+    sumWage                   = sparse(double(IW(idx)),1,double(WW(idx)));
+    countWage                 = sparse(double(IW(idx)),1,1);
+    [Temp,~,sumWage]          = find(sumWage);
+    [~,~,countWage]           = find(countWage);
+    WageCount                 = zeros(C.NumAgentsSim,2);
+    WageCount(Temp,1)         = sumWage./countWage;
+    WageCount(Temp,2)         = countWage;
+    errors                    = WW(idx) - WageCount(IW(idx),1);
+    NumM                      = numel(errors);
+    VecTemp                   = errors;
+    
+    
     Temp                      = WageCount(:,2) > 0;
     NumW                      = sum(Temp);
     xyWageCount(icnt + 1 : icnt + NumW,1) = SimO.iNames(Temp);
     xyWageCount(icnt + 1 : icnt + NumW,2) = ij;
     xyWageCount(icnt + 1 : icnt + NumW,3) = WageCount(Temp,1);
     xyWageCount(icnt + 1 : icnt + NumW,4) = WageCount(Temp,2);
-    Vector(icntV + 1: icntV + NumM)       = VecTemp(1:NumM);
+    Vector(icntV + 1: icntV + NumM)      = VecTemp(1:NumM);
     icnt    = icnt + NumW;
     icntV   = icntV + NumM;
   end
