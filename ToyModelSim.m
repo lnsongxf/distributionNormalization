d5b70f7c8def5c29e310034dc89e668410f0943a
tzuohann <tzuohann@gmail.com>
2017-07-17 19:59:22 -0400

    getMoments applied to ranking seems to be better. think about the right setup.

--- ToyModelSim.m
+++ ToyModelSim.m
@@ -7,11 +7,11 @@ function ToyModelSim()
   TrueParam2.Beta1              = 1.3;
   TrueParam2.Beta2              = 1.7;
   TrueParam2.ProdParam          = 0.6;
-  numWorkers                    = 100000;
+  numWorkers                    = 50000;
   numGrid                       = 1000;
-  measurementError              = 0.3;
+  measurementError              = 0.1;
   simRounds                     = 10;
-  randomStarts                  = 10;
+  randomStarts                  = 20;
   
   %Construct the "true" data
   %Name is worker id
@@ -78,19 +78,31 @@ function ToyModelSim()
   m2UsingProd1_NonP      = getMoments(interp1(DomainProd1,nonParProd1,Dist2.RankNorm));
   m2UsingDist1_NonP      = getMoments(interp1(DomainProd2,nonParProd2,Dist1.Rank));
   
+  for i1 = 1:randomStarts
+    startPoint = rand(1,6);
+    startPoint = 1.2*[TrueParam1.Beta1 TrueParam1.Beta2 TrueParam1.ProdParam TrueParam2.Beta1 TrueParam2.Beta2 TrueParam2.ProdParam] + (startPoint-0.5);
+    startPoint_vec(i1,:) = startPoint;
+  end
+  
   %Parametric Approach
   %COnsider outcomes of starting at randomly chosen points around the truth
   simMoments             = @(X) simulateWages(X,simRounds,numWorkers);
   
+  options                           = optimoptions('fminunc','Algorithm','quasi-newton');
+  options.Display                   = 'iter-detailed';
+  options.StepTolerance             = 1e-6;
+  options.FiniteDifferenceStepSize  = 1e-2;
+  options.OptimalityTolerance       = 1e-6;
+  
   for i1 = 1:randomStarts
-    startPoint = rand(1,6);
-    startPoint = [1 1 1 1 0 0] + startPoint;
-    paramTogether = fminunc(@(X) sum((simMoments(X(1:3)) - m1_TRUE).^2) + ...
-      sum((simMoments(X(4:6)) - m2_TRUE).^2), startPoint);
-    paramEst1                  = paramTogether(1:3);
-    paramEst2                  = paramTogether(4:6);
-    paramEst1_vec(i1,:)        = paramTogether(1:3);
-    paramEst2_vec(i1,:)        = paramTogether(4:6);
+    startPoint = startPoint_vec(i1,:);
+    parFunc = @(X) sum((simMoments(X(1:3)) - m1_TRUE).^2) + ...
+      sum((simMoments(X(4:6)) - m2_TRUE).^2);
+    paramTogether = fminunc(parFunc, startPoint,options);
+    paramEst1                      = paramTogether(1:3)
+    paramEst2                      = paramTogether(4:6)
+    paramEst1_vec(i1,:)            = paramTogether(1:3);
+    paramEst2_vec(i1,:)            = paramTogether(4:6);
     m2UsingProd1_Par_vec(i1,:)     = getMoments(betarnd(paramEst2(1),paramEst2(2),numWorkers,1).^paramEst1(3));
     m2UsingDist1_Par_vec(i1,:)     = getMoments(betarnd(paramEst1(1),paramEst1(2),numWorkers,1).^paramEst2(3));
   end
@@ -100,36 +112,59 @@ function ToyModelSim()
   % True Moment: (Dist1.Skill(I1)-Dist2.Skill(I2)) = 0
   % Sample Moment: betainv(Dist1.Rank(I1),X(1),X(2))-betainv(Dist2.Rank(I2),X(4),X(5)) = 0
   for i1 = 1:randomStarts
-    startPoint = rand(1,6);
-    startPoint = [1 1 1 1 0 0] + startPoint;
-    paramTogether = fminunc(@(X) sum((simulateWages(X(1:3),simRounds,numWorkers) - m1_TRUE).^2) + ...
+    startPoint = startPoint_vec(i1,:);
+    parFunc = @(X) sum((simulateWages(X(1:3),simRounds,numWorkers) - m1_TRUE).^2) + ...
       sum((simulateWages(X(4:6),simRounds,numWorkers) - m2_TRUE).^2) + ...
       sum((getMoments(betainv(Dist1.Rank(ismember(Dist1.Name,OverLap)),X(1),X(2))) - ...
-      getMoments(betainv(Dist2.Rank(ismember(Dist2.Name,OverLap)),X(4),X(5)))).^2),...
-      startPoint);
-    paramEst1_R                     = paramTogether(1:3);
-    paramEst2_R                     = paramTogether(4:6);
-    paramEst1_vec(i1,:)             = paramTogether(1:3);
-    paramEst2_vec(i1,:)             = paramTogether(4:6);
+      getMoments(betainv(Dist2.Rank(ismember(Dist2.Name,OverLap)),X(4),X(5)))).^2);
+    paramTogether = fminunc(parFunc,...
+      startPoint,options);
+    paramEst1_R                     = paramTogether(1:3)
+    paramEst2_R                     = paramTogether(4:6)
+    paramEst1R_vec(i1,:)            = paramTogether(1:3);
+    paramEst2R_vec(i1,:)            = paramTogether(4:6);
     m2UsingProd1_ParR_vec(i1,:)     = getMoments(betarnd(paramEst2_R(1),paramEst2_R(2),numWorkers,1).^paramEst1_R(3));
     m2UsingDist1_ParR_vec(i1,:)     = getMoments(betarnd(paramEst1_R(1),paramEst1_R(2),numWorkers,1).^paramEst2_R(3));
   end
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   
-  m2UsingProd1_Par = mean(m2UsingProd1_Par_vec);
-  m2UsingDist1_Par = mean(m2UsingDist1_Par_vec);
+  m2UsingProd1_Par  = mean(m2UsingProd1_Par_vec);
+  m2UsingDist1_Par  = mean(m2UsingDist1_Par_vec);
   m2UsingProd1_ParR = mean(m2UsingProd1_ParR_vec);
   m2UsingDist1_ParR = mean(m2UsingDist1_ParR_vec);
   
-  disp('Decompositions with true parameters.')
-  disp(sprintf('True              & %7.4f & %7.4f & %7.4f & %7.4f \\\\ ',             m2UsingProd1_TRUE(1),m2UsingProd1_TRUE(2),m2UsingProd1_TRUE(3),m2UsingProd1_TRUE(4)));
-  disp(sprintf('NonParametric     & %7.4f & %7.4f & %7.4f & %7.4f \\\\ ',    m2UsingProd1_NonP(1),m2UsingProd1_NonP(2),m2UsingProd1_NonP(3),m2UsingProd1_NonP(4)));
-  disp(sprintf('Parametric        & %7.4f & %7.4f & %7.4f & %7.4f \\\\ ',       m2UsingProd1_Par(1),m2UsingProd1_Par(2),m2UsingProd1_Par(3),m2UsingProd1_Par(4)));
-  disp(sprintf('Parametric + Rank & %7.4f & %7.4f & %7.4f & %7.4f \\\\ ',m2UsingProd1_ParR(1),m2UsingProd1_ParR(2),m2UsingProd1_ParR(3),m2UsingProd1_ParR(4)));
-  disp(sprintf('True              & %7.4f & %7.4f & %7.4f & %7.4f \\\\ ',             m2UsingDist1_TRUE(1),m2UsingDist1_TRUE(2),m2UsingDist1_TRUE(3),m2UsingDist1_TRUE(4)));
-  disp(sprintf('NonParametric     & %7.4f & %7.4f & %7.4f & %7.4f \\\\ ',    m2UsingDist1_NonP(1),m2UsingDist1_NonP(2),m2UsingDist1_NonP(3),m2UsingDist1_NonP(4)));
-  disp(sprintf('Parametric        & %7.4f & %7.4f & %7.4f & %7.4f \\\\ ',       m2UsingDist1_Par(1),m2UsingDist1_Par(2),m2UsingDist1_Par(3),m2UsingDist1_Par(4)));
-  disp(sprintf('Parametric + Rank & %7.4f & %7.4f & %7.4f & %7.4f \\\\ ',m2UsingDist1_ParR(1),m2UsingDist1_ParR(2),m2UsingDist1_ParR(3),m2UsingDist1_ParR(4)));
-  save
+  save withgetMomentsBigSampleSpecBias.mat
+  
+%     disp('Decompositions with true parameters.')
+%     disp(sprintf('True              & %7.4f & %7.4f & %7.4f & %7.4f \\\\ ',             m2UsingProd1_TRUE(1),m2UsingProd1_TRUE(2),m2UsingProd1_TRUE(3),m2UsingProd1_TRUE(4)));
+%     disp(sprintf('NonParametric     & %7.4f & %7.4f & %7.4f & %7.4f \\\\ ',    m2UsingProd1_NonP(1),m2UsingProd1_NonP(2),m2UsingProd1_NonP(3),m2UsingProd1_NonP(4)));
+%     disp(sprintf('Parametric        & %7.4f & %7.4f & %7.4f & %7.4f \\\\ ',       m2UsingProd1_Par(1),m2UsingProd1_Par(2),m2UsingProd1_Par(3),m2UsingProd1_Par(4)));
+%     disp(sprintf('Parametric + Rank & %7.4f & %7.4f & %7.4f & %7.4f \\\\ ',m2UsingProd1_ParR(1),m2UsingProd1_ParR(2),m2UsingProd1_ParR(3),m2UsingProd1_ParR(4)));
+%     disp(sprintf('True              & %7.4f & %7.4f & %7.4f & %7.4f \\\\ ',             m2UsingDist1_TRUE(1),m2UsingDist1_TRUE(2),m2UsingDist1_TRUE(3),m2UsingDist1_TRUE(4)));
+%     disp(sprintf('NonParametric     & %7.4f & %7.4f & %7.4f & %7.4f \\\\ ',    m2UsingDist1_NonP(1),m2UsingDist1_NonP(2),m2UsingDist1_NonP(3),m2UsingDist1_NonP(4)));
+%     disp(sprintf('Parametric        & %7.4f & %7.4f & %7.4f & %7.4f \\\\ ',       m2UsingDist1_Par(1),m2UsingDist1_Par(2),m2UsingDist1_Par(3),m2UsingDist1_Par(4)));
+%     disp(sprintf('Parametric + Rank & %7.4f & %7.4f & %7.4f & %7.4f \\\\ ',m2UsingDist1_ParR(1),m2UsingDist1_ParR(2),m2UsingDist1_ParR(3),m2UsingDist1_ParR(4)));
+%   
+
+%   disp('Decompositions with true parameters.')
+%   disp(sprintf('True              & %7.4f & %7.4f  \\\\ ',             m2UsingProd1_TRUE(1),m2UsingProd1_TRUE(2)));
+%   disp(sprintf('NonParametric     & %7.4f & %7.4f  \\\\ ',    m2UsingProd1_NonP(1),m2UsingProd1_NonP(2)));
+%   disp(sprintf('Parametric        & %7.4f & %7.4f  \\\\ ',       m2UsingProd1_Par(1),m2UsingProd1_Par(2)));
+%   disp(sprintf('Parametric + Rank & %7.4f & %7.4f  \\\\ ',m2UsingProd1_ParR(1),m2UsingProd1_ParR(2)));
+%   disp(sprintf('True              & %7.4f & %7.4f  \\\\ ',             m2UsingDist1_TRUE(1),m2UsingDist1_TRUE(2)));
+%   disp(sprintf('NonParametric     & %7.4f & %7.4f  \\\\ ',    m2UsingDist1_NonP(1),m2UsingDist1_NonP(2)));
+%   disp(sprintf('Parametric        & %7.4f & %7.4f  \\\\ ',       m2UsingDist1_Par(1),m2UsingDist1_Par(2)));
+%   disp(sprintf('Parametric + Rank & %7.4f & %7.4f  \\\\ ',m2UsingDist1_ParR(1),m2UsingDist1_ParR(2)));
+%   
+   disp('Decompositions with true parameters.')
+    disp(sprintf('True              & %7.4f & %7.4f & %7.4f  \\\\ ',             m2UsingProd1_TRUE(1),m2UsingProd1_TRUE(2),m2UsingProd1_TRUE(3)));
+    disp(sprintf('NonParametric     & %7.4f & %7.4f & %7.4f  \\\\ ',    m2UsingProd1_NonP(1),m2UsingProd1_NonP(2),m2UsingProd1_NonP(3)));
+    disp(sprintf('Parametric        & %7.4f & %7.4f & %7.4f  \\\\ ',       m2UsingProd1_Par(1),m2UsingProd1_Par(2),m2UsingProd1_Par(3)));
+    disp(sprintf('Parametric + Rank & %7.4f & %7.4f & %7.4f  \\\\ ',m2UsingProd1_ParR(1),m2UsingProd1_ParR(2),m2UsingProd1_ParR(3)));
+    disp(sprintf('True              & %7.4f & %7.4f & %7.4f  \\\\ ',             m2UsingDist1_TRUE(1),m2UsingDist1_TRUE(2),m2UsingDist1_TRUE(3)));
+    disp(sprintf('NonParametric     & %7.4f & %7.4f & %7.4f  \\\\ ',    m2UsingDist1_NonP(1),m2UsingDist1_NonP(2),m2UsingDist1_NonP(3)));
+    disp(sprintf('Parametric        & %7.4f & %7.4f & %7.4f  \\\\ ',       m2UsingDist1_Par(1),m2UsingDist1_Par(2),m2UsingDist1_Par(3)));
+    disp(sprintf('Parametric + Rank & %7.4f & %7.4f & %7.4f  \\\\ ',m2UsingDist1_ParR(1),m2UsingDist1_ParR(2),m2UsingDist1_ParR(3)));
+  
 end
--- getMoments.m
+++ getMoments.m
@@ -2,5 +2,4 @@ function m = getMoments(X)
   m(1) = mean(X);
   m(2) = moment(X,2);
   m(3) = moment(X,3);
-  m(4) = moment(X,4);
 end
